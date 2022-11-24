65e0f1a9283452e33ac62536e5fa982469b5ddef
AlefMemTav <190115548@aluno.unb.br>
2022-11-24 19:20:56 -0300

    Exemple 2

---
+++ exemple2.s
@@ -0,0 +1,43 @@
+.data
+	msg1: .asciiz "\nDigite um numero: "
+	msg2: .asciiz "\nO maior valor: "
+
+.text
+main:
+
+	li $v0, 4					#Escrever msg1
+	la $a0, msg1
+	syscall
+
+	li $v0, 5					#Ler inteiro
+	syscall
+	add $t0, $v0, $zero
+
+	li $v0, 4
+	la $a0, msg1
+	syscall
+
+	li $v0, 5
+	syscall
+	add $t1, $v0, $zero
+
+	li $v0, 4
+	la $a0, msg2
+	syscall
+
+	bge $t0, $t1, se
+	j senao
+	j fimse
+
+	se:
+	li $v0, 1
+	add $a0, $t0, $zero
+	syscall
+
+	senao:
+	li $v0, 1
+	add $a0, $t1, $zero
+	syscall
+	
+	fimse:
+
