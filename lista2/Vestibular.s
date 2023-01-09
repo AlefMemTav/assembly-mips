54e543c31d55901264c835e202264cc434498be2
AlefMemTav <190115548@aluno.unb.br>
2023-01-08 21:20:56 -0300

    Lendo duas strings

--- lista2/Vestibular.s
+++ lista2/Vestibular.s
@@ -1,9 +1,9 @@
 # $t0 <= n + 1
 # stringSize inicializado com 0
 # stringInput inicializado com 0
-# stringInputDois inicializado com 0
 # $t1 <= stringInput
-# $t2 <= stringInputDois
+# $t2 i contador do while
+# $t3 j contador de acertos
 
 .data
 	stringInput: .space 80
@@ -16,21 +16,36 @@
 		li $v0, 5 # Chamar read_int
 		syscall # Ler
 		
-		add $t0, $v0, 1 # Armazenar tamanho lido somado a 1 em $t0
+		add $t0, $v0, 2 # Armazenar tamanho lido + 2 bytes em $t0
 		
 		sw $t0, stringSize($zero) # Armazenar $t0 em stringSize
 
     # Ler string gabarito 
 		li $v0, 8 # Chamar read_str
-		la $a0, stringInput # Carregar endereco da string em $a0
+		la $a0, stringInput # Carregar endereco de stringInput em $a0
     lw $a1, stringSize # Carregar tamanho em $a1
-		add $t1, $a0, $zero
 		syscall # Ler str
 
+		add $t1, $a0, $zero # Salvar endereço da string em $t1
+
 		# Ver stringInput
+		la $a0, stringInput
     li $v0, 4
-    la $a0, stringInput
     syscall
+
+		# Ler string prova 
+		li $v0, 8 # Chamar read_str
+		la $a0, stringInputDois # Carregar endereco de stringInput em $a0
+    lw $a1, stringSize # Carregar tamanho em $a1
+		syscall # Ler str
+
+		add $t2, $a0, $zero # Salvar endereço da string em $t2
+
+		# Ver stringInputDois
+		la $a0, stringInputDois
+    li $v0, 4
+    syscall
+
 
 	#Chamar exit
 	li $v0, 10		
