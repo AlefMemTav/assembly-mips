c89b91d755d954d21917f199f05e7c15e0d735bc
AlefMemTav <190115548@aluno.unb.br>
2023-01-08 22:14:49 -0300

    Quase finalizado

--- lista2/Vestibular.s
+++ lista2/Vestibular.s
@@ -1,53 +1,83 @@
-# $t0 <= n + 1
-# stringSize inicializado com 0
-# stringInput inicializado com 0
+# $t0 <= n + 2
 # $t1 <= stringInput
-# $t2 i contador do while
-# $t3 j contador de acertos
+# $t2 <= stringInputDois
+# $t4 <= i
+
+# stringSize <= n + 2
 
 .data
 	stringInput: .space 80
 	stringInputDois: .space 80
 	stringSize: .word 0 
+	str: .asciiz"\n"
 .text
 
 	main:
-		# Ler tamanho n dado pelo usuario
+		li $t4, 0 # Contador
+		
 		li $v0, 5 # Chamar read_int
 		syscall # Ler
 		
-		add $t0, $v0, 2 # Armazenar tamanho lido + 2 bytes em $t0
-		
+		add $t0, $v0, 2 # Armazenar em $t0 o tamanho lido + 2 bytes 
+
 		sw $t0, stringSize($zero) # Armazenar $t0 em stringSize
-
-    # Ler string gabarito 
+	
+		# GABARITO
+  
+		# Ler gabarito 
+	
 		li $v0, 8 # Chamar read_str
-		la $a0, stringInput # Carregar endereco de stringInput em $a0
-    lw $a1, stringSize # Carregar tamanho em $a1
+		la $a0, stringInput # Carregar stringInput em $a0
+    lw $a1, stringSize # Carregar stringSize em $a1
 		syscall # Ler str
+	
+		# Salvar gabarito
 
-		add $t1, $a0, $zero # Salvar endereço da string em $t1
+		add $t1, $a0, $zero # Salvar string1 em $t1
 
-		# Ver stringInput
+		# Ver gabarito
+
 		la $a0, stringInput
     li $v0, 4
     syscall
 
-		# Ler string prova 
+		# PROVA
+
+		# Ler prova
+
 		li $v0, 8 # Chamar read_str
-		la $a0, stringInputDois # Carregar endereco de stringInput em $a0
-    lw $a1, stringSize # Carregar tamanho em $a1
+		la $a0, stringInputDois # Carregar stringInputDois em $a0
+    lw $a1, stringSize # Carregar stringSize em $a1
 		syscall # Ler str
 
-		add $t2, $a0, $zero # Salvar endereço da string em $t2
+		# Salvar prova
 
-		# Ver stringInputDois
+		add $t2, $a0, $zero # Salvar string2 em $t2
+
+		# Ver prova
+
 		la $a0, stringInputDois
     li $v0, 4
     syscall
 
+		while:
+		beq $t0, $t4, fim  
+			lb $a0, 0($t1) # Ler byte da string
+			li $v0, 11 # Chamar print_char
+			syscall # Imprimir
+			
+			addi $t1, 1 # Avancar byte da string
+			addi $t4, $t4, 1 # i++
+
+			# Imprimir quebra de linha
+			la $a0, str	# Atribuir quebra de linha em $a0	
+			li $v0, 4 #Chamar print_str
+			syscall #Imprimir
+
+			j while
+		fim:
 
 	#Chamar exit
-	li $v0, 10		
+	li $v0, 10	
 	#Encerrar
-	syscall	
+	syscall
